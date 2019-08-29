4b3a7bafab38b32fcd5e940ef9212c243afe9eb7
Casey Brannon <cbrannon412@gmail.com>
2019-08-29 15:36:39 -0400

    header

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,8 +4,7 @@ import './NavBar.css';
 
 const NavBar = (props) => {
     let nav = props.user ?
-        <div >
-
+        <div className="divGuy">
             <div>
                 <Link to='' onClick={props.handleLogOut}>Log Out</Link>
             </div>
@@ -14,20 +13,20 @@ const NavBar = (props) => {
             </div>
         </div>
         :
-        <div >
-            <div>
+        <div  >
+            <div className="divGuy" >
                 <Link to='/login'>LOG IN</Link>
             </div>
             <div>
                 <Link to='/signup' >SIGN UP</Link>
             </div>
-        </div>;
+        </div>
 
     return (
 
         <div className='NavBar'>
             <div>
-                <div className='NavBar-img'>X</div>
+                <div style={{margin: '-15px 0 0 0'}}><h3>Hacker News</h3></div>
             </div>
             <div>
                 {nav}
